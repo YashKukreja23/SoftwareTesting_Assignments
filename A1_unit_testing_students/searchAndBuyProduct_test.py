afc2e94920b3de9509bff760209ccaeeba7dd516
JorgeAlonsoSuS <jorge.alonso2002@opendeusto.es>
2023-11-24 12:59:00 +0100

    Search and Buy products some tests done

--- A1_unit_testing_students/searchAndBuyProduct_test.py
+++ A1_unit_testing_students/searchAndBuyProduct_test.py
@@ -5,27 +5,42 @@ from products import searchAndBuyProduct
 import csv
 import shutil
 import os
+import products
+import checkout_and_payment
 import unittest.mock
 from unittest.mock import patch
-@pytest.fixture()
+@pytest.fixture
 def copy_csv_file():
     shutil.copyfile('products.csv', 'copy_products.csv')
     yield 'copy_products.csv'
     os.remove('copy_products.csv')
 
-@patch('builtins.input', side_effect=['valid_username', 'valid_password', 'all', 'Y'])
-@patch('products.display_csv_as_table')
-@patch('products.checkoutAndPayment')
-def test_search_and_buy_product_valid_inputs(mock_checkout, mock_display):
-    searchAndBuyProduct()
-    # Add assertions to check the expected behavior based on valid inputs
+@pytest.fixture
+def display_csv_as_table_mock(mocker):
+    return mocker.patch('products.display_csv_as_table', return_value = None)
 
-@patch('builtins.input', side_effect=['invalid_username', 'invalid_password', 'all', 'Y'])
-def test_search_and_buy_product_invalid_login():
-    # Test for invalid login input
-    with pytest.raises(Exception):  # Adjust this to the actual exception type raised in your code
-        searchAndBuyProduct()
+@pytest.fixture
+def display_filtered_table_mock(mocker):
+    return mocker.patch('products.display_filtered_table', return_value = None)
 
-def test_function():
-    with mock.patch.object(__builtins__, 'input', lambda: 'some_input'):
-        assert searchAndBuyProduct() == 'expected_output'
+@pytest.fixture
+def checkout_and_payment_mock(mocker):
+    return mocker.patch('checkout_and_payment.checkoutAndPayment', return_value = None)
+
+def test_display_csv_as_table(mocker, copy_csv_file):
+    m = mocker.MagicMock(return_value = None)
+    mocker.patch('products.display_csv_as_table', m)
+    products.display_csv_as_table(copy_csv_file)
+    m.assert_called_once()
+
+def test_display_filtered_table(mocker, copy_csv_file):
+    m = mocker.MagicMock(return_value = None)
+    mocker.patch('products.display_filtered_table', m)
+    products.display_filtered_table(copy_csv_file, 'Apple')
+    m.assert_called_once()
+
+def test_checkout_and_payment(mocker):
+    m = mocker.MagicMock(return_value = None)
+    mocker.patch('checkout_and_payment.checkoutAndPayment', m)
+    checkout_and_payment.checkoutAndPayment()
+    m.assert_called_once()
