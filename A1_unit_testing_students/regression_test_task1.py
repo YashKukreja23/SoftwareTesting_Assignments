716b8a8af40bce805b358f69ae239b49c829d546
JorgeAlonsoSuS <jorge.alonso2002@opendeusto.es>
2023-12-31 14:04:42 +0100

    Task 1

---
+++ A1_unit_testing_students/regression_test_task1.py
@@ -0,0 +1,5 @@
+import pytest
+import pytest_catchlog
+
+from display_filtered_table_test import copy_csv_file, test_header, test_rows, test_search, test_exists, test_correct_type, test_missing_input
+from display_table_as_csv_test import test_exists, test_not_exists, test_invalid_bool, test_invalid_float, test_invalid_file_input, copy_csv_file
